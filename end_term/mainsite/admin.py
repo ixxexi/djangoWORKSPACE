68ff2ef87c4425728e21f63ca0b1fad3d91d6372
ixxexi_mac <steven68516291@gmail.com>
2024-06-20 03:26:12 +0800

    -

--- end_term/mainsite/admin.py
+++ end_term/mainsite/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 from django.contrib import admin
-from .models import Auction, Bid
+from .models import Auctions, Bids
 
-admin.site.register(Auction)
-admin.site.register(Bid)
+admin.site.register(Auctions)
+admin.site.register(Bids)
